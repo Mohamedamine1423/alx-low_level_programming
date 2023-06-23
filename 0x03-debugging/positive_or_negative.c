5e53c4172f6d7a1b7f5f9518a153da32253379a7
Mohamedamine1423 <125656205+Mohamedamine1423@users.noreply.github.com>
2023-06-23 02:44:24 +0100

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,19 +2,12 @@
 
 void positive_or_negative(int i)
 {
-	int n;
-	n = i;
-
 	if (n > 0)
-	{
 		printf("%d is positive\n", i);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
+		
+	else if (n < 0)
 		printf("%d is negative\n", i);
-	}
+		
+	else
+		printf("%d is zero\n", i);
 }
