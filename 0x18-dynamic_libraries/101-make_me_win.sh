1a3ea9e2e2c12ffad604feef7e59d7899df76583
Mohamedamine1423 <aminetbandi19@gmail.com>
2023-09-22 09:49:51 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Mohamedamine1423/alx-low_level_programming/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp/ https://github.com/Mohamedamine1423/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
